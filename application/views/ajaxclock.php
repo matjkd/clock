7cacceeeed32cd6f7380a207174568fcdcd4024d
Mat Sadler <mat@redstudio.co.uk>
2014-10-22 11:55:42 +0100

    set interval for weather update
    updates every ten minutes without screen refresh

--- application/views/ajaxclock.php
+++ application/views/ajaxclock.php
@@ -177,22 +177,31 @@
 	</script>
 	        <script type="text/javascript">
 	        // Docs at http://simpleweatherjs.com
-$(document).ready(function() {
+	        
+	        
+$(document).ready(function() {  
+	
+	
+  getWeather(); //Get the initial weather.
+  setInterval(getWeather, 600000); //Update the weather every 10 minutes.
+});	        
+
+function getWeather() {
   $.simpleWeather({
     location: '<?=$weather?>',
-    woeid: '',
-    unit: 'c',
+    unit: '<?=strtolower($temp)?>',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+     html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
      
-  
       $("#weather").html(html);
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-});
+}
+	        
+
 	        
 	            $(document).ready(function() {
 	            		
